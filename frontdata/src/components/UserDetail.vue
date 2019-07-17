e65fd28b7fa8f61af726e56511bed345e8eb9a00
DoubleGoer <khs4637@naver.com>
2019-07-17 13:08:51 +0900

    frontend

--- frontdata/src/components/UserDetail.vue
+++ frontdata/src/components/UserDetail.vue
@@ -2,15 +2,28 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>이름 : {{nameOfchild}}</p>
+    <p>{{sayHello}}</p>
   </div>
+
 </template>
 
 <script>
   export default{
-    props:['nameOfchild'],
+    props:{
+      nameOfchild :{
+        type : String,
+        requierd : true,
+        default : "Lego"
+      }
+    },
     data(){
       return{
 
+      }
+    },
+    computed: {
+      sayHello(){
+        return this.nameOfchild + '안녕하세요!'
       }
     }
   }
